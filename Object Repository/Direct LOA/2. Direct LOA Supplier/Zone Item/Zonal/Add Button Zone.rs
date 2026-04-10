966d46e2bbe42e029dfa5a89ceaa119560fd5b40
hadishafiq <hadishafiq@CDC-CBJ0786LL.pst.local>
2026-04-10 17:53:40 +0800

    commit

---
+++ Object Repository/Direct LOA/2. Direct LOA Supplier/Zone Item/Zonal/Add Button Zone.rs
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Add Button Zone</name>
+   <tag></tag>
+   <elementGuidId>e25c516f-7df6-4d86-a611-3b783637a894</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//button[contains(@onclick,'zonePopupGeneral') and .//span[normalize-space()='Add']]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
---
+++ Object Repository/Direct LOA/2. Direct LOA Supplier/Zone Item/Zonal/Zone Name.rs
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Zone Name</name>
+   <tag></tag>
+   <elementGuidId>085faad9-ebdf-44ba-8670-de420f98e789</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//*[@id=&quot;_scCreateManualSourcing_WAR_NGePportlet_:form:zoneNameId&quot;]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
