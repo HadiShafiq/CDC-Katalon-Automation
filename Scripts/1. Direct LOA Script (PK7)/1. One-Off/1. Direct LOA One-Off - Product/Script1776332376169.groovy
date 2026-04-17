f5444826ad1afda7d034c58547c87170d82324e5
hadishafiq <hadishafiq@CDC-CBJ0786LL.pst.local>
2026-04-17 11:41:59 +0800

    c

--- Scripts/1. Direct LOA Script (PK7)/1. One-Off/1. Direct LOA One-Off - Product/Script1776332376169.groovy
+++ Scripts/1. Direct LOA Script (PK7)/1. One-Off/1. Direct LOA One-Off - Product/Script1776332376169.groovy
@@ -230,13 +230,13 @@ def selectDropdownByIndex(TestObject dropdownObj, def indexFromData) {
  * - disable password manager prompts
  * ========================= */
 
-/* PATH HADI
+/* PATH HADI*/
 String chromeBinary = "C:\\Users\\hadishafiq\\Downloads\\chrome-win64\\chrome-win64\\chrome.exe"
-String chromeDriverPath = "C:\\Users\\hadishafiq\\Downloads\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe" */
-
+String chromeDriverPath = "C:\\Users\\hadishafiq\\Downloads\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe" 
+/* PATH Atikah
 String chromeBinary = "C:\\Users\\nurul.atikah\\Documents\\CDC - Work\\Automation\\Automation Testing Browser FIles\\chrome-win64\\chrome-win64\\chrome.exe"
 String chromeDriverPath = "C:\\Users\\nurul.atikah\\Documents\\CDC - Work\\Automation\\Automation Testing Browser FIles\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe"
-
+*/
 System.setProperty("webdriver.chrome.driver", chromeDriverPath)
 
 String userDataDir = Files.createTempDirectory("katalon-cft").toString()
@@ -1018,9 +1018,9 @@ WebUI.comment("✅ Captured LOA No: " + loaNo)
  * Purpose:
  * - append LOA number and message into same Excel file
  * ========================= */
-/* PATH HADI 
-String filePath = "C:\\Users\\hadishafiq\\Desktop\\PrepData\\Direct_LOA_Non-Zonal_One-Off_PK7_Product_AP_201_2026.xlsx"*/
-String filePath = "C:\\Users\\nurul.atikah\\Documents\\CDC - Work\\Automation\\Test Data\\Application_NO\\Direct_LOA_Non-Zonal_One-Off_PK7_Product_AP_201_2026.xlsx"
+/* PATH HADI */
+String filePath = "C:\\Users\\hadishafiq\\Desktop\\PrepData\\Direct_LOA_Non-Zonal_One-Off_PK7_Product_AP_201_2026.xlsx"
+//String filePath = "C:\\Users\\nurul.atikah\\Documents\\CDC - Work\\Automation\\Test Data\\Application_NO\\Direct_LOA_Non-Zonal_One-Off_PK7_Product_AP_201_2026.xlsx"
 String now = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date())
 
 def path = Paths.get(filePath)
