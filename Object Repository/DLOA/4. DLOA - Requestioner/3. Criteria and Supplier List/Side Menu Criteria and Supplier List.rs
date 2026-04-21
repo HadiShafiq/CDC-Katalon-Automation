b9d7a043d185ec88f42f402d0888be7f2cf42f04
nurul.atikah <nurul.atikah@CDC-CBJ0792LL.pst.local>
2026-04-21 17:26:01 +0800

    Side menu criteria & supplier

---
+++ Object Repository/DLOA/4. DLOA - Requestioner/3. Criteria and Supplier List/Side Menu Criteria and Supplier List.rs
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Side Menu Criteria and Supplier List</name>
+   <tag></tag>
+   <elementGuidId>a0522445-1d39-4bf3-a052-4ffb2517ad0d</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//a[span[normalize-space(.)='Criteria and Supplier List']]</value>
+      </entry>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
