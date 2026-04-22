6b00a46153e38140fbdb4d729932baf9fcf09b2c
nurul.atikah <nurul.atikah@CDC-CBJ0792LL.pst.local>
2026-04-23 02:26:07 +0800

    tikah

--- Scripts/2. DLOA/5. Prepare LOA/Script1772514742076.groovy
+++ Scripts/2. DLOA/5. Prepare LOA/Script1772514742076.groovy
@@ -24,7 +24,7 @@ import java.io.FileOutputStream
 import com.kms.katalon.core.testobject.ConditionType
 
 /* =========================
- * Calendar Picker Date
+ * Calendar Picker Date - tikah
  * ========================= */
 
 def pickDate(String yyyyMmDd) {
