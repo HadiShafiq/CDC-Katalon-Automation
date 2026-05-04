7314870044c0aac9b94feaca422b4ed969af5fdd
nurul.atikah <nurul.atikah@CDC-CBJ0792LL.pst.local>
2026-05-04 17:48:03 +0800

    AGREEMENT UPLOAD 2

---
+++ Object Repository/FD and Agreement/Side Menu/Agreement/Side Menu Agreement Contract Document.rs
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Side Menu Agreement Contract Document</name>
+   <tag></tag>
+   <elementGuidId>2569cd3a-b2b6-409e-994a-948950abc5f0</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//span[normalize-space()='Agreement Contract Document']</value>
+      </entry>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
