3801bbe96efcd0fd40f84c47e8e29e97dc746a7f
nurul.atikah <nurul.atikah@CDC-CBJ0792LL.pst.local>
2026-05-04 12:36:57 +0800

    date - performance bond

---
+++ Object Repository/FD and Agreement/Performance Bond/Click Icon Date.rs
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Click Icon Date</name>
+   <tag></tag>
+   <elementGuidId>e084febf-d115-43b6-874c-f13000718110</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//span[contains(@class,'ui-icon-calendar')]/ancestor::button</value>
+      </entry>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
