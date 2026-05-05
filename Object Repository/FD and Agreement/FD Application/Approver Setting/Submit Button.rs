57ead83fe58fdce87a4a78c2e7defb4b1fcc422c
nurul.atikah <nurul.atikah@CDC-CBJ0792LL.pst.local>
2026-05-05 17:36:06 +0800

    AGREEMENT - DONE 2

---
+++ Object Repository/FD and Agreement/FD Application/Approver Setting/Submit Button.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Submit Button</name>
+   <tag></tag>
+   <elementGuidId>6a2f73ea-1f65-45e4-b790-4c425875a04a</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//button[.//span[normalize-space()='Submit']]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
