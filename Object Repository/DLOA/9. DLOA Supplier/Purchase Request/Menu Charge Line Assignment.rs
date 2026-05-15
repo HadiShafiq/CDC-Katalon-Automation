da69acda207ca91bfa83af65fa50f7458eeaa9df
nurul.atikah <nurul.atikah@CDC-CBJ0792LL.pst.local>
2026-05-15 11:23:08 +0800

    Purchase request 2

---
+++ Object Repository/DLOA/9. DLOA Supplier/Purchase Request/Menu Charge Line Assignment.rs
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Menu Charge Line Assignment</name>
+   <tag></tag>
+   <elementGuidId>bf50b4dc-f805-4dec-b9ad-2285355a345d</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//li[contains(@class,'ui-tabmenuitem') and .//span[normalize-space()='Charge Line Assignment']]</value>
+      </entry>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
