f53545007e11bbfa0a30121f21d60f4eb04635a7
hadishafiq <hadishafiq@CDC-CBJ0786LL.pst.local>
2026-05-18 12:56:40 +0800

    add to cart

---
+++ Object Repository/DP - Add To Cart/1. Add to Cart Requestioner/8. Procurement Type Category.rs
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>8. Procurement Type Category</name>
+   <tag></tag>
+   <elementGuidId>9f98a29f-d976-4a32-bedc-d894f119aa25</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//*[contains(@id,'procTypeCategory')]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
