5aa787b43ad3fc3ee6b38eff5383d3fddd673cb9
hadishafiq <hadishafiq@CDC-CBJ0786LL.pst.local>
2026-05-18 12:36:14 +0800

    Add to Cart

---
+++ Object Repository/DP - Add To Cart/1. Add to Cart Requestioner/6. Click Proceed Button.rs
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>6. Click Proceed Button</name>
+   <tag></tag>
+   <elementGuidId>3c80eebe-1962-4c91-81a8-cbfd2c32f5c7</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//button[contains(@id,'proceedBtn')]//span[normalize-space()='Proceed']</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
