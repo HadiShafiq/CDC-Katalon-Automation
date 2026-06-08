a45f015f29d8a8db61d88fa237f82f7a62e2ee48
nurul.atikah <nurul.atikah@CDC-CBJ0792LL.pst.local>
2026-06-08 14:57:17 +0800

    PAYMENT MATCH 1

---
+++ Object Repository/DP - Add To Cart/Payment Match/Click Sign WO GPKI.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Click Sign WO GPKI</name>
+   <tag></tag>
+   <elementGuidId>ac0dd501-b0f4-48a0-91fd-a87921dfd986</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//button[.//span[normalize-space()='Sign W/O GPKI']]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
