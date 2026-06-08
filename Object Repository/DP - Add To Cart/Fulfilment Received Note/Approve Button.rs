913bef35601b9a030f77a69e6e7c1f2b81ffb955
nurul.atikah <nurul.atikah@CDC-CBJ0792LL.pst.local>
2026-06-08 09:50:47 +0800

    SECOND

---
+++ Object Repository/DP - Add To Cart/Fulfilment Received Note/Approve Button.rs
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Approve Button</name>
+   <tag></tag>
+   <elementGuidId>010944b2-cfa6-495f-9a46-61c31cdf119c</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//button[normalize-space()='Approve']</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
---
+++ Object Repository/DP - Add To Cart/Fulfilment Received Note/Click Sign WO GPKI.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Click Sign WO GPKI</name>
+   <tag></tag>
+   <elementGuidId>de089d32-e190-435e-ab5b-58edb3d144e7</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//button[.//span[normalize-space()='Sign W/O GPKI']]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
