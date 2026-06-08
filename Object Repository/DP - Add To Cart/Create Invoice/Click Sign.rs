c7e3ad7587d4ba11223a3bbe29746accb6f30a4a
nurul.atikah <nurul.atikah@CDC-CBJ0792LL.pst.local>
2026-06-08 11:47:20 +0800

    invoice

---
+++ Object Repository/DP - Add To Cart/Create Invoice/Click Sign.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Click Sign</name>
+   <tag></tag>
+   <elementGuidId>f4a6d3cc-efcf-4b9a-957f-5903df728e1f</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>(//button[.//span[text()='Sign']])[2]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
---
+++ Object Repository/DP - Add To Cart/Create Invoice/Submit Button.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Submit Button</name>
+   <tag></tag>
+   <elementGuidId>0ac0748f-ced1-439b-a5ec-082cd36a0769</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//button[.//span[normalize-space()='Submit']]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
