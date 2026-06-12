11bc543740eb99110433992532f8543ce0c6daaa
hadishafiq <hadishafiq@CDC-CBJ0786LL.pst.local>
2026-06-12 15:42:41 +0800

    ok

---
+++ Object Repository/DP - Add To Cart/Pending Delivery List/Delivery Amount (RM).rs
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Delivery Amount (RM)</name>
+   <tag></tag>
+   <elementGuidId>26174cc7-ce22-4e1d-a95a-b2ea2c2d8166</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//span[contains(@id,'doDelAddress:0:doItems:0:delAmountTxt') and contains(@class,'delAmountTxt')]/input</value>
+      </entry>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
