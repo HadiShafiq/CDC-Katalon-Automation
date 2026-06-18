d794048d50e2a579bcbde8c4646966f90efb755f
hadishafiq <hadishafiq@CDC-CBJ0786LL.pst.local>
2026-06-18 12:29:51 +0800

    commit

--- Scripts/4. Fulfillment/4.2 Contract Fulfillment/1. Contract List/Script1781745946435.groovy
+++ Scripts/4. Fulfillment/4.2 Contract Fulfillment/1. Contract List/Script1781745946435.groovy
@@ -533,36 +533,42 @@ c(findTestObject('Object Repository/DP - Add To Cart/Contract List/Tickbox - I d
 //selectDropdownByIndex(findTestObject('Object Repository/DP - Add To Cart/Contract List/Dropdown Supplier Branch'),ddSupplier)
 def selectSupplierBranchIfEnabledByIndex(int optionIndex) {
 
-    // Main Supplier Branch dropdown
-    TestObject dropdown = new TestObject('Supplier Branch Dropdown')
-    dropdown.addProperty('xpath', ConditionType.EQUALS,
-        "//*[@id='_flContractRequest_WAR_NGePportlet_:form:suppBranch']/div[3]"
+    // optionIndex is 1-based
+    // 1 = first option
+    // 2 = second option
+    // 3 = third option
+
+    TestObject disabledDropdown = new TestObject('Supplier Branch Disabled')
+    disabledDropdown.addProperty('xpath', ConditionType.EQUALS,
+        "//div[@id='_flContractRequest_WAR_NGePportlet_:form:suppBranch' and contains(@class,'ui-state-disabled')]"
     )
 
-    WebUI.waitForElementPresent(dropdown, 10)
+    TestObject disabledSelect = new TestObject('Supplier Branch Disabled Select')
+    disabledSelect.addProperty('xpath', ConditionType.EQUALS,
+        "//select[@id='_flContractRequest_WAR_NGePportlet_:form:suppBranch_input' and @disabled='disabled']"
+    )
 
-    // Check disabled
-    String dropdownClass = WebUI.getAttribute(dropdown, 'class', FailureHandling.OPTIONAL)
+    boolean isDisabledByClass = WebUI.verifyElementPresent(disabledDropdown, 2, FailureHandling.OPTIONAL)
+    boolean isDisabledBySelect = WebUI.verifyElementPresent(disabledSelect, 2, FailureHandling.OPTIONAL)
 
-    if (dropdownClass != null && dropdownClass.contains('ui-state-disabled')) {
+    if (isDisabledByClass || isDisabledBySelect) {
         println('Supplier Branch dropdown is disabled. Skip selection.')
         return
     }
 
-    // Click dropdown trigger
     TestObject trigger = new TestObject('Supplier Branch Trigger')
     trigger.addProperty('xpath', ConditionType.EQUALS,
-        "//*[@id='_flContractRequest_WAR_NGePportlet_:form:suppBranch']/div[3]"
+        "//div[@id='_flContractRequest_WAR_NGePportlet_:form:suppBranch']//div[contains(@class,'ui-selectonemenu-trigger')]"
     )
 
     WebUI.waitForElementClickable(trigger, 10)
     WebUI.click(trigger)
 
-    // Select option by index
-    // optionIndex is 0-based, XPath is 1-based, so +1 is needed
+    WebUI.delay(1)
+
     TestObject option = new TestObject('Supplier Branch Option Index ' + optionIndex)
     option.addProperty('xpath', ConditionType.EQUALS,
-        "(//div[contains(@class,'ui-selectonemenu-panel') and contains(@style,'display: block')]//li[contains(@class,'ui-selectonemenu-item')])[${optionIndex + 1}]"
+        "(//div[@id='_flContractRequest_WAR_NGePportlet_:form:suppBranch_panel']//li[contains(@class,'ui-selectonemenu-item')])[${optionIndex}]"
     )
 
     WebUI.waitForElementVisible(option, 10)
@@ -571,4 +577,4 @@ def selectSupplierBranchIfEnabledByIndex(int optionIndex) {
     println('Supplier Branch selected by index: ' + optionIndex)
 }
 
-selectSupplierBranchIfEnabledByIndex(1)
+selectSupplierBranchIfEnabledByIndex(2)
